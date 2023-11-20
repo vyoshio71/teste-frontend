fd1a5bad463c69be87d9ae69af1521698f1ef66e
vyoshio71 <yoshio.victor@hotmail.com>
2023-11-20 20:36:10 -0300

    create: create context and functions for add products to cart and remove

---
+++ src/Contexts/CartContext.tsx
@@ -0,0 +1,71 @@
+import { createContext, useContext, ReactNode, useState } from "react";
+
+interface Product {
+  id: number;
+  name: string;
+  brand: string;
+  description: string;
+  photo: string;
+  price: string;
+}
+
+interface CartContextProps {
+  cartItems: CartItem[];
+  addToCart: (product: Product) => void;
+  handleQuantityChange: (productId: number, change: number) => void;
+}
+
+interface CartProviderProps {
+  children: ReactNode;
+}
+
+interface CartItem extends Product {
+  quantity: number;
+}
+
+const CartContext = createContext<CartContextProps | undefined>(undefined);
+
+export function CartProvider({ children }: CartProviderProps) {
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  const addToCart = (product: Product) => {
+    const existingItem = cartItems.find((item) => item.id === product.id);
+
+    if (existingItem) {
+      handleQuantityChange(existingItem.id, 1);
+    } else {
+      setCartItems((prevCartItems) => [
+        ...prevCartItems,
+        { ...product, quantity: 1 },
+      ]);
+    }
+  };
+
+  const handleQuantityChange = (productId: number, change: number) => {
+    setCartItems((prevCartItems) =>
+      prevCartItems
+        .map((item) =>
+          item.id === productId
+            ? { ...item, quantity: Math.max(item.quantity + change, 0) }
+            : item
+        )
+        .filter((item) => item.quantity > 0)
+    );
+  };
+
+  return (
+    <CartContext.Provider
+      value={{ cartItems, addToCart, handleQuantityChange }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export function useCart() {
+  const context = useContext(CartContext);
+  if (!context) {
+    throw new Error("useCart must be used within a CartProvider");
+  }
+  return context;
+}
