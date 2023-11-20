2c0d93331d0421265007a9ed5a6a276ac9d09a1b
vyoshio71 <yoshio.victor@hotmail.com>
2023-11-20 20:35:27 -0300

    feat: create loading page for project

---
+++ src/app/components/LoadingIndicator/LoadingIndicator.tsx
@@ -0,0 +1,9 @@
+// components/LoadingIndicator.tsx
+import React from "react";
+import "./LoadingIndicator.scss";
+
+const LoadingIndicator: React.FC = () => {
+  return <div className="loading"></div>;
+};
+
+export default LoadingIndicator;
