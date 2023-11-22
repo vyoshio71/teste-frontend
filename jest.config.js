a706085c5a0be3cd81fb2cc1f7c65b90c0bae0a3
vyoshio71 <yoshio.victor@hotmail.com>
2023-11-21 21:25:44 -0300

    feat: config for jest teste

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.[jt]sx?$": "babel-jest",
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+};
